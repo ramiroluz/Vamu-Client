6c86aacb21cc2f86dbf37bf55eb95c6b2b321a05
Ramiro Batista da Luz <ramiroluz@gmail.com>
2011-06-15 10:42:06 -0300

    first commit

---
+++ src/vamu_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# main.py
+# Copyright (C) Ramiro Batista da Luz 2011 <ramiroluz@gmail.com>
+# 
+# vamu-client is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# vamu-client is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+print "Hello World!"
